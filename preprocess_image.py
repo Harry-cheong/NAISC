19a4047c5e81176d0bbdebcedb3540bd8f22d7b8
WANGCHEN722 <52946865+WANGCHEN722@users.noreply.github.com>
2023-01-26 17:37:58 +0800

    Add files via upload
    its just all the models plus some testing files

---
+++ preprocess_image.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from peekingduck.pipeline.nodes.model.jde import Node
+import numpy as np
+
+class Preprocessor:
+    def __init__(self,**kwargs):
+        self.tracker=Node(kwargs).model.tracker
+    def __call__(self,image):
+        image_array=np.flip(np.asarray(image),2)
+        predicts=self.tracker.track_objects_from_image(image_array)
+        bboxes=np.clip(predicts[0],0,1)
+        feats={track.track_id: track.features[-1] for track in self.tracker.tracked_stracks}
+        x_bboxes=np.rint(image_array.shape[1]*bboxes[:,0::2]).astype(int)
+        y_bboxes=np.rint(image_array.shape[0]*bboxes[:,1::2]).astype(int)
+        images=[]
+        for (left,right),(up,down) in zip(x_bboxes,y_bboxes):
+            images.append((left,up,right,down))
+        return images,[feats[f] for f in predicts[1]]
