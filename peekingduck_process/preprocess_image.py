d6efc57ed6d984ef5343d9be72458b92a6401266
WANGCHEN722 <wangchensemail@gmail.com>
2023-02-03 22:58:18 +0800

    swapped face recognition model

--- peekingduck_process/preprocess_image.py
+++ peekingduck_process/preprocess_image.py
@@ -1,22 +1,27 @@
-from peekingduck.pipeline.nodes.model.jde import Node
+from peekingduck.pipeline.nodes.model.mtcnn import Node
 import numpy as np
 
 class Preprocessor:
     def __init__(self,**kwargs):
-        self.tracker=Node(kwargs).model.tracker
+        self.detector=Node(kwargs).model.detector
     def __call__(self,image):
         image=image.convert('RGB')
         image_array=np.flip(np.asarray(image),2)
-        predicts=self.tracker.track_objects_from_image(image_array)
+        predicts=self.detector.predict_object_bbox_from_image(image_array)
 
-        if predicts == ([],[],[]):
+        if not predicts[0].any():
             return [],[]
-
+        #print(predicts)
         bboxes=np.clip(predicts[0],0,1)
-        feats={track.track_id: track.features[-1] for track in self.tracker.tracked_stracks}
+        landmarks=np.concatenate([(yx if ind == 1 else np.flip(yx,1)) for ind,yx in enumerate(predicts[2])],axis=1)
         x_bboxes=np.rint(image_array.shape[1]*bboxes[:,0::2]).astype(int)
         y_bboxes=np.rint(image_array.shape[0]*bboxes[:,1::2]).astype(int)
         images=[]
         for (left,right),(up,down) in zip(x_bboxes,y_bboxes):
             images.append(image.crop((left,up,right,down)))
-        return images,[feats[f] for f in predicts[1]]
+        return images,landmarks
+
+if __name__ == '__main__':
+    from PIL import Image
+    p=Preprocessor()
+    print(p(Image.open(r'..\imgDataset\2.jpg')))
